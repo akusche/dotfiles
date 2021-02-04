780ba32103da70828bc09807bc9299e4e3bf75a6
akusche <akusche@icloud.com>
2021-02-04 09:53:39 +0100

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -11,24 +11,24 @@ brew install pandoc
 # Install MacOS Applications
 brew cask install spotify
 brew cask install google-chrome
-brew cask install boxcryptor
+#brew cask install boxcryptor
 brew cask install firefox
-brew cask install virtualbox
-brew cask install virtualbox-extension-pack
+#brew cask install virtualbox
+#brew cask install virtualbox-extension-pack
 brew cask install sublime-text
-brew cask install sublime-merge
-brew cask install skype-for-business
+#brew cask install sublime-merge
+#brew cask install skype-for-business
 brew cask install skype
 brew cask install teamviewer
 brew cask install pdfsam-basic
-brew cask install karabiner-elements
+#brew cask install karabiner-elements
 brew cask install dropbox
 brew cask install vlc
-brew cask install karabiner-elements
-brew cask install r
+#brew cask install r
 brew cask install zoom-in
-brew cask install microsoft-teams
-brew cask install dbeaver-community
+#brew cask install microsoft-teams
+#brew cask install dbeaver-community
+brew install --cask zotero #Literaturverwaltungstool
 
 # Install Source Code Pro Font
 brew tap homebrew/cask-fonts
