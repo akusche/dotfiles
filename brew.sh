feaf3674c0bb79ffe23719f3447937f811b933d3
Alexander Kusche <akusche@icloud.com>
2022-12-06 22:33:53 +0100

    neuer mac aufgesetzt

--- brew.sh
+++ brew.sh
@@ -4,36 +4,28 @@
 brew update
 
 # Install Brew Packages
-brew install python
 brew install tree
 brew install pandoc
 brew install diff-pdf
 brew install htop			#zeigt wunderbar die aktuell laufenden Prozesse des Systems
 brew install dfc 			#zeigt die Festplattenbelegung
-brew install neofetch		#zeigt die Informationen zum System an.
+brew install neofetch			#zeigt die Informationen zum System an.
 brew install pyenv 			#verwaltet mehre Python Versionen auf dem OS 
-brew install docker-edge	#microservices docker images
 
 # Install MacOS Applications
 brew install --cask google-chrome
 brew install --cask boxcryptor
 brew install --cask firefox
-brew install --cask virtualbox
-brew install --cask virtualbox-extension-pack
 brew install --cask sublime-text
 brew install --cask sublime-merge
-brew install --cask skype-for-business
-brew install --cask skype
 brew install --cask teamviewer
 brew install --cask pdfsam-basic
 brew install --cask karabiner-elements
 brew install --cask vlc
-brew install --cask karabiner-elements
 brew install --cask zoom-in
 brew install --cask microsoft-teams
-brew install --cask dbeaver-community
 brew install --cask iterm2
-brew install --cask zotero #Literaturverwaltungstool
+brew install --cask zotero 		#Literaturverwaltungstool
 brew install --cask portfolioperformance
 brew install --cask libreoffice
 brew install --cask angry-ip-scanner
--- sublime.sh
+++ sublime.sh
@@ -12,7 +12,6 @@ cp -r settings/Anaconda.sublime-settings ~/Library/Application\ Support/Sublime\
 cp -r settings/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
 
 # Create Python Build Systems
-cp -r settings/Python-2.sublime-build ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
 cp -r settings/Python-3.sublime-build ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
 
 # Custom Settings For Theme
