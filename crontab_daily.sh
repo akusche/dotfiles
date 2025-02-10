61dd67d507faeee25ee3e8328bca07505c2688c7
Alexander Kusche <akusche@icloud.com>
2025-02-10 12:00:48 +0100

    delete by birth date

--- crontab_daily.sh
+++ crontab_daily.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/usr/bin/find /Users/alex/Downloads/* -atime +10d -exec rm -rf {} \; >> ~/crontab_daily.log 2>&1
+/usr/bin/find /Users/alex/Downloads/* -Btime +10d -exec rm -rf {} \; >> ~/crontab_daily.log 2>&1
 /opt/homebrew/bin/brew update >> ~/crontab_daily.log 2>&1
 /opt/homebrew/bin/brew upgrade >> ~/crontab_daily.log 2>&1
 /opt/homebrew/bin/brew upgrade --cask >> ~/crontab_daily.log 2>&1
